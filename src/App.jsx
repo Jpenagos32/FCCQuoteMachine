b598b322776c08c3811d0e99517aa7c549df2517
jpenagos32 <jpenagosdev@gmail.com>
2024-04-03 17:46:04 -0500

    Agregando la informacion de desarrollador y cambiando los iconos

--- src/App.jsx
+++ src/App.jsx
@@ -12,43 +12,57 @@ function App() {
   const randomNumber = Math.floor(Math.random() * quotes.length);
   const [number, setNumber] = useState(randomNumber);
 
-  const handleClick = () => {
-    setNumber(randomNumber);
-  };
 
   const { author, quote } = quotes[number];
-
   const quoteToTweet = `"${quote}"; - ${author}`
 
+  const handleClick = () => {
+    const random = Math.floor(Math.random() * quotes.length);
+    setNumber(random);
+  };
+
   return (
-    <div className='container' id='quote-box'>
-      <IconContext.Provider value={{ className: 'quote-icon' }}>
-        <h2 className='text-center ' id='text'>
-          <FaQuoteLeft />
-          {quote}
-          <FaQuoteRight />
-        </h2>
-      </IconContext.Provider>
-      <p id='author'> - {author} </p>
-
-      <div className='buttons'>
-
-        <button onClick={handleClick} id='new-quote'>
-          New Quote
-        </button>
-        <IconContext.Provider value={{ className: 'twitter-share-button' }}>
-          <a
-            href={`https://twitter.com/intent/tweet?text=${quoteToTweet}`}
-            target='_blank'
-            rel='noreferrer'
-            id='tweet-quote'
-            title='tweet this quote'
-          >
-            <FaSquareXTwitter />
-          </a>
+    <>
+      <div className='container' id='quote-box'>
+        <IconContext.Provider value={{ className: 'quote-icon' }}>
+          <h2 className='text-center ' id='text'>
+            <FaQuoteLeft />
+            {quotes[number].quote}
+            <FaQuoteRight />
+          </h2>
         </IconContext.Provider>
+        <p id='author'> - {quotes[number].author} </p>
+
+        <div className='buttons'>
+
+          <button type='submit' onClick={handleClick} id='new-quote'>
+            New Quote
+          </button>
+          <IconContext.Provider value={{ className: 'twitter-share-button' }}>
+            <a
+              href={`https://twitter.com/intent/tweet?text=${quoteToTweet}`}
+              target='_blank'
+              rel='noreferrer'
+              id='tweet-quote'
+              title='tweet this quote'
+            >
+              <FaSquareXTwitter />
+            </a>
+          </IconContext.Provider>
+        </div>
       </div>
-    </div>
+
+      <div className='info'>
+        <p className='info-name p-margin'>
+          Julian Andres Penagos
+        </p>
+        <a target='_blank' className='info-mail p-margin' title='Send me an e-mail' href="mailto:jpenagosdev@gmail.com">
+          jpenagosdev@gmail.com
+        </a>
+        <a className='info-github p-margin' target='_blank' href="https://github.com/Jpenagos32">Github: @jpenagos32</a>
+      </div>
+    </>
+
   );
 }
 
